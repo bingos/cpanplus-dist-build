5d7aa070927e2417cabf5ccbd94ae99ffa4a3e49
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2014-01-08 08:47:19 +0000

    Release engineering for 0.76

--- lib/CPANPLUS/Dist/Build/Constants.pm
+++ lib/CPANPLUS/Dist/Build/Constants.pm
@@ -11,7 +11,7 @@ BEGIN {
     require Exporter;
     use vars    qw[$VERSION @ISA @EXPORT];
 
-    $VERSION    = '0.74';
+    $VERSION    = '0.76';
     @ISA        = qw[Exporter];
     @EXPORT     = qw[ BUILD_DIR BUILD CPDB_PERL_WRAPPER];
 }
